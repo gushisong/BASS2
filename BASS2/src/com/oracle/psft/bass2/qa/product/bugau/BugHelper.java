d1602ffcb4233da1ff98c5af2ec83bd4edc512de
xiaoleis.song <xiaolei.song@oracle.com>
2017-11-26 23:28:47 -0700

    A

---
+++ BASS2/src/com/oracle/psft/bass2/qa/product/bugau/BugHelper.java
@@ -0,0 +1,15 @@
+package com.oracle.psft.bass2.qa.product.bugau;
+
+public class BugHelper {
+	
+	private String bugDBHome;
+	private String ssoUserName;
+	private String ssoPassword;
+	
+	public BugHelper(String bugDBHome,String ssoUserName,String ssoPassword) {
+		this.bugDBHome=bugDBHome;
+		this.ssoUserName=ssoUserName;
+		this.ssoPassword=ssoPassword;
+	}
+
+}
