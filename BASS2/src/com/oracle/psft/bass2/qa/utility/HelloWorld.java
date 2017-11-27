837aeb38dd5f6f5f38bf624f6dbaba56f2bfb588
xiaoleis.song <xiaolei.song@oracle.com>
2017-11-26 20:57:31 -0700

    AA

--- BASS2/src/com/oracle/psft/bass2/qa/utility/HelloWorld.java
+++ BASS2/src/com/oracle/psft/bass2/qa/utility/HelloWorld.java
@@ -1,9 +1,13 @@
 package com.oracle.psft.bass2.qa.utility;
 
+import org.openqa.selenium.WebDriver;
+
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Test");
+		
+		WebDriver driver=LunchBrowser.LunchChrome();
 	}
 
 }
