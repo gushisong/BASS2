5744973f45efe08c6eadd3b79b1fe407dbdcdb30
xiaoleis.song <xiaolei.song@oracle.com>
2017-11-26 20:58:05 -0700

    A

---
+++ BASS2/src/com/oracle/psft/bass2/qa/utility/Constants.java
@@ -0,0 +1,20 @@
+package com.oracle.psft.bass2.qa.utility;
+
+public class Constants {
+	
+	
+	public static final String BrowserPath="C:\\OracleImage\\selenium\\webDriver\\chromedriver.exe";
+	
+	//public static final String URL="http://jqueryui.com/resources/demos/droppable/default.html";
+	
+	public static final String URL="http://toolsqa.com/automation-practice-switch-windows/";
+	
+	public static final String UserName="xiaoleisong";
+	
+	public static final String Password="4883131";
+	
+	public static final String Path_TestData="C://Users//xiaolson//eclipse-workspace//OnlineStore//src//testData//";
+	
+	public static final String File_TestData="TestData.xlsx";
+
+}
---
+++ BASS2/src/com/oracle/psft/bass2/qa/utility/LunchBrowser.java
@@ -0,0 +1,21 @@
+package com.oracle.psft.bass2.qa.utility;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LunchBrowser {
+	
+	public static WebDriver LunchChrome()
+	{
+		String exePath=Constants.BrowserPath;
+		System.setProperty("webdriver.chrome.driver",exePath);
+		WebDriver driver=new ChromeDriver();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get(Constants.URL);
+		return driver;
+	}
+
+}
