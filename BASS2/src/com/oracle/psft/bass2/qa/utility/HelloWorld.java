e0a44d85c59f6941a7e65e96dca5c6c4814333be
xiaoleis.song <xiaolei.song@oracle.com>
2017-11-26 20:48:34 -0700

    AA

--- BASS2/src/com/oracle/psft/bass2/qa/utility/HelloWorld.java
+++ BASS2/src/com/oracle/psft/bass2/qa/utility/HelloWorld.java
@@ -3,6 +3,7 @@ package com.oracle.psft.bass2.qa.utility;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Test");
 	}
 
 }
